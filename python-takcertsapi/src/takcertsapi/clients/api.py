4f477c73dfecdacaa3b2b4917fa47af9df74644e
Eero af Heurlin <eero.afheurlin@iki.fi>
2022-12-02 23:00:48 +0200

    Implement the POST api that calls the client creation script

--- python-takcertsapi/src/takcertsapi/clients/api.py
+++ python-takcertsapi/src/takcertsapi/clients/api.py
@@ -1,6 +1,9 @@
 """API implementation"""
+import asyncio
+import shlex
 from typing import List
 from pathlib import Path
+import logging
 
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
@@ -8,7 +11,7 @@ from fastapi.responses import FileResponse
 from ..config import INSTANCE as CONFIG
 from .schema import ListClients, ClientPkg
 
-
+LOGGER = logging.getLogger(__name__)
 CLIENT_ROUTER = APIRouter()
 
 
@@ -22,9 +25,26 @@ async def read_clients() -> ListClients:
 
 
 @CLIENT_ROUTER.get("/api/v1/clients/{clientname}", tags=["clients"], response_class=FileResponse)
-async def read_user(clientname: str) -> FileResponse:
+async def read_client(clientname: str) -> FileResponse:
     """Get a specific client zip pkg"""
     pth = CONFIG.client_zips_location / Path(f"{clientname}.zip")
     if not pth.exists():
         raise HTTPException(status_code=404, detail="Client package not found")
+    return FileResponse(pth)
+
+
+@CLIENT_ROUTER.post("/api/v1/clients/{clientname}", tags=["clients"], response_class=FileResponse)
+async def create_client(clientname: str) -> FileResponse:
+    """Get a specific client zip pkg"""
+    pth = CONFIG.client_zips_location / Path(f"{clientname}.zip")
+    if pth.exists():
+        raise HTTPException(status_code=409, detail="Client package already exists")
+    cmd = f"CLIENT_CERT_NAME={shlex.quote(clientname)} {CONFIG.zip_script_location}"
+    proc = await asyncio.create_subprocess_shell(cmd)
+    stdout, stderr = await proc.communicate()
+    if proc.returncode != 0 or not pth.exists():
+        LOGGER.error("command '{}' exited with code {}".format(cmd, proc.returncode))
+        LOGGER.info("stdout: {!r}".format(stdout))
+        LOGGER.info("stderr: {!r}".format(stderr))
+        raise HTTPException(status_code=500, detail="Package creation failed. Contact server administration")
     return FileResponse(pth)
--- python-takcertsapi/tests/test_clients.py
+++ python-takcertsapi/tests/test_clients.py
@@ -1,4 +1,5 @@
 """Test the clients endpoint"""
+import pytest
 from fastapi.testclient import TestClient
 
 from takcertsapi.clients.schema import ListClients
@@ -32,8 +33,19 @@ def test_post_exists(client: TestClient, temp_cert_path: None) -> None:
     assert resp.status_code == 409
 
 
-def test_post(client: TestClient, temp_cert_path: None) -> None:
+@pytest.mark.parametrize("clientname", ["koira16", "ääkköstesti"])
+def test_post(client: TestClient, temp_cert_path: None, clientname: str) -> None:
     """Test creating new pkg"""
     _ = temp_cert_path
-    resp = client.post("/api/v1/clients/koira16")
+    resp = client.post(f"/api/v1/clients/{clientname}")
     assert resp.status_code == 200
+    # Check that it's added to list too
+    resp2 = client.get("/api/v1/clients")
+    assert resp2.status_code == 200
+    data = ListClients(**resp2.json())
+    assert data.items
+    found = False
+    for item in data.items:
+        if item.name == clientname:
+            found = True
+    assert found
