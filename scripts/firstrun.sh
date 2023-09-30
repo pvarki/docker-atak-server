502519a0ad3f5a3383327f113e88e0a1e15344fc
Toni Syvänen <toni@syvanen.fi>
2023-09-30 16:35:28 +0300

    * Clean out old files * The firstrun.sh is now idempotent * Cert and Logs volumes return (for now?) and are properly symlinked * Startup scripts now combined with if statement to select what process starts up * Works locally (at least the WebUI opens on port 8080)

--- scripts/firstrun.sh
+++ scripts/firstrun.sh
@@ -1,64 +1,63 @@
 #!/usr/bin/env -S /bin/bash
-TR=/mnt/tak
+TR=/opt/tak
 CR=${TR}/certs
 CONFIG=${TR}/data/CoreConfig.xml
-if [ -f /mnt/tak/firstrun.done ]
-then
-  echo "First run already done"
-else
-  set -e
-  
-  cp -r /opt/tak/. /mnt/tak/
 
-  # Remove hardcoded country code
-  sed -i.orig "s/COUNTRY=US/COUNTRY=\${COUNTRY}/g" ${CR}/cert-metadata.sh
-  # Override some distribution scripts outright since doing it with sed is too painful
+set -e
+
+# Remove hardcoded country code
+sed -i.orig "s/COUNTRY=US/COUNTRY=\${COUNTRY}/g" ${CR}/cert-metadata.sh
+# Override some distribution scripts outright since doing it with sed is too painful
+if [[ ! "/opt/scripts/makeCert.sh" ]];then
   mv /opt/scripts/makeCert.sh ${CR}/
-
-  # Seed initial certificate data if necessary
-  if [[ ! -d "${TR}/data/certs" ]];then
-    mkdir -p "${TR}/data/certs"
-  fi
-  if [[ -z "$(ls -A "${TR}/data/certs")" ]];then
-    echo Copying initial certificate configuration
-    cp -R ${TR}/certs/* ${TR}/data/certs/
-  else
-    echo Using existing certificates.
-  fi
-
-  # Move original certificate data and symlink to certificate data in data dir
-  mv ${TR}/certs ${TR}/certs.orig
-  ln -s "${TR}/data/certs" "${TR}/certs"
-
-  # Symlink the log directory
-  ln -s "${TR}/data/logs" "${TR}/logs"
-
-  cd ${CR}
-
-  if [[ ! -f "${CR}/files/root-ca.pem" ]];then
-    CAPASS=${CA_PASS} bash makeRootCa.sh --ca-name "${CA_NAME}"
-  else
-    echo Using existing root CA.
-  fi
-
-  if [[ ! -f "${CR}/files/takserver.pem" ]];then
-    CAPASS=${CA_PASS} PASS="${TAKSERVER_CERT_PASS}" bash makeCert.sh server takserver
-  else
-    echo Using existing takserver certificate.
-  fi
-
-  if [[ ! -f "${CR}/files/${ADMIN_CERT_NAME}.pem" ]];then
-    CAPASS=${CA_PASS} PASS="${ADMIN_CERT_PASS}" bash makeCert.sh client "${ADMIN_CERT_NAME}"
-  else
-    echo Using existing ${ADMIN_CERT_NAME} certificate.
-  fi
-
-  chmod -R 777 ${TR}/data/
-
-  echo "Wait for postgres"
-  WAITFORIT_TIMEOUT=60 /usr/bin/wait-for-it.sh ${POSTGRES_ADDRESS}:5432 -- true
-  echo "Init db"
-  java -jar ${TR}/db-utils/SchemaManager.jar -url jdbc:postgresql://${POSTGRES_ADDRESS}:5432/${POSTGRES_DB} -user ${POSTGRES_USER} -password ${POSTGRES_PASSWORD} upgrade
-
-  date -u +"%Y%m%dT%H%M" >/mnt/tak/firstrun.done
 fi
+
+
+# Seed initial certificate data if necessary
+if [[ ! -d "${TR}/data/certs" ]];then
+  mkdir -p "${TR}/data/certs"
+fi
+if [[ -z "$(ls -A "${TR}/data/certs")" ]];then
+  echo Copying initial certificate configuration
+  cp -R ${TR}/certs/* ${TR}/data/certs/
+else
+  echo Using existing certificates.
+fi
+
+# Move original certificate data and symlink to certificate data in data dir
+if [[ ! -L "${TR}/certs"  ]];then
+  mv ${TR}/certs ${TR}/certs.orig
+  ln -s "${TR}/data/certs/" "${TR}/certs"
+fi
+
+# Symlink the log directory
+if [[ ! -L "${TR}/certs"  ]];then
+  ln -s "${TR}/data/logs/" "${TR}/logs"
+fi
+
+cd ${CR}
+
+if [[ ! -f "${CR}/files/root-ca.pem" ]];then
+  CAPASS=${CA_PASS} bash makeRootCa.sh --ca-name "${CA_NAME}"
+else
+  echo Using existing root CA.
+fi
+
+if [[ ! -f "${CR}/files/takserver.pem" ]];then
+  CAPASS=${CA_PASS} PASS="${TAKSERVER_CERT_PASS}" bash makeCert.sh server takserver
+else
+  echo Using existing takserver certificate.
+fi
+
+if [[ ! -f "${CR}/files/${ADMIN_CERT_NAME}.pem" ]];then
+  CAPASS=${CA_PASS} PASS="${ADMIN_CERT_PASS}" bash makeCert.sh client "${ADMIN_CERT_NAME}"
+else
+  echo Using existing ${ADMIN_CERT_NAME} certificate.
+fi
+
+chmod -R 777 ${TR}/data/
+
+echo "Wait for postgres"
+WAITFORIT_TIMEOUT=60 /usr/bin/wait-for-it.sh ${POSTGRES_ADDRESS}:5432 -- true
+echo "Init db"
+java -jar ${TR}/db-utils/SchemaManager.jar -url jdbc:postgresql://${POSTGRES_ADDRESS}:5432/${POSTGRES_DB} -user ${POSTGRES_USER} -password ${POSTGRES_PASSWORD} upgrade
