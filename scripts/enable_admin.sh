e7c4d306ee6a7d44f8d65be9e3cea348bf4d1b97
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-01-28 18:36:53 +0200

    load env before waiting for DB

--- scripts/enable_admin.sh
+++ scripts/enable_admin.sh
@@ -2,6 +2,8 @@
 set -e
 TR=/opt/tak
 CONFIG=${TR}/data/CoreConfig.xml
+cd ${TR}
+. ./setenv.sh
 
 # Wait for server start
 echo "enable_admin: Waiting for db"
@@ -10,6 +12,4 @@ echo "enable_admin: Waiting for TAK server"
 WAITFORIT_TIMEOUT=60 /usr/bin/wait-for-it.sh localhost:8089 -- true
 
 echo "enable_admin: Making sure ${ADMIN_CERT_NAME} user is in place"
-cd ${TR}
-. ./setenv.sh
 TAKCL_CORECONFIG_PATH="${CONFIG}" java -jar /opt/tak/utils/UserManager.jar certmod -A "/opt/tak/data/certs/files/${ADMIN_CERT_NAME}.pem"
