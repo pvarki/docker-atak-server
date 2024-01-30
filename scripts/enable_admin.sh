718b27d131e218d776b085fd638bc6370f1ff2e8
Eero af Heurlin <eero.afheurlin@iki.fi>
2024-01-30 17:11:03 +0200

    wait only 2s, if the server is not available waiting for longer will only read to other parts timing out

--- scripts/enable_admin.sh
+++ scripts/enable_admin.sh
@@ -5,11 +5,8 @@ CONFIG=${TR}/data/CoreConfig.xml
 cd ${TR}
 . ./setenv.sh
 
-# Wait for server start
-#echo "enable_admin: Waiting for db"
-#WAITFORIT_TIMEOUT=30 /usr/bin/wait-for-it.sh ${POSTGRES_ADDRESS}:5432 -- true
 echo "enable_admin: Waiting for TAK server"
-WAITFORIT_TIMEOUT=90 /usr/bin/wait-for-it.sh localhost:8089 -- true
+WAITFORIT_TIMEOUT=2 /usr/bin/wait-for-it.sh localhost:8089 -- true
 
 echo "enable_admin: Making sure ${ADMIN_CERT_NAME} user is in place"
 TAKCL_CORECONFIG_PATH="${CONFIG}" java -jar /opt/tak/utils/UserManager.jar certmod -A "/opt/tak/data/certs/files/${ADMIN_CERT_NAME}.pem"
