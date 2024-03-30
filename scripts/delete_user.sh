119f21c6c61f0826bf22bea1d4da3c1937a0a301
Ari Karhunen <ari.karhunen@tietoevry.com>
2024-03-30 18:55:06 +0200

    Default user group non anon

--- scripts/delete_user.sh
+++ scripts/delete_user.sh
@@ -5,4 +5,8 @@ CONFIG=${TR}/data/CoreConfig.xml
 
 cd ${TR}
 . ./setenv.sh
+
+
+# First set user to "jail" group and then actually delete the user. Removing the user doesn't make the client disconnect the active session.
+TAKCL_CORECONFIG_PATH="${CONFIG}" java -jar /opt/tak/utils/UserManager.jar certmod -g jail "/opt/tak/data/certs/files/${USER_CERT_NAME}.pem"
 TAKCL_CORECONFIG_PATH="${CONFIG}" java -jar /opt/tak/utils/UserManager.jar certmod -D "/opt/tak/data/certs/files/${USER_CERT_NAME}.pem"
